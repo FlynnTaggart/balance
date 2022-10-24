7a18e3b423dcce5acf0c008d4129ee4325a63197
Mikhail <mr1302@ya.ru>
2022-10-24 16:08:41 +0300

    feat: add zap logger

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -12,9 +12,28 @@ import (
 	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4/log/zapadapter"
 	"github.com/jackc/pgx/v4/pgxpool"
 	_ "github.com/joho/godotenv/autoload"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
+
+func initializeLogger() *zap.Logger {
+	config := zap.NewDevelopmentEncoderConfig()
+	config.EncodeTime = zapcore.ISO8601TimeEncoder
+	fileEncoder := zapcore.NewJSONEncoder(config)
+	consoleEncoder := zapcore.NewConsoleEncoder(config)
+	logFile, _ := os.OpenFile("./logs/server.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	writer := zapcore.AddSync(logFile)
+	defaultLogLevel := zapcore.InfoLevel
+	core := zapcore.NewTee(
+		zapcore.NewCore(fileEncoder, writer, defaultLogLevel),
+		zapcore.NewCore(consoleEncoder, zapcore.AddSync(os.Stdout), defaultLogLevel),
+	)
+	return zap.New(core, zap.AddCaller(), zap.AddStacktrace(zapcore.ErrorLevel))
+}
 
 func main() {
 	app := fiber.New()
@@ -24,33 +43,27 @@ func main() {
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_DATABASE"),
-		os.Getenv("DB_PORT"),
-	)
+		os.Getenv("DB_PORT"))
 
-	//db, err := gorm.Open(postgres.Open(dsn))
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//sqlDB, err := db.DB()
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//sqlDB.SetMaxIdleConns(10)
-	//sqlDB.SetMaxOpenConns(100)
-	//sqlDB.SetConnMaxLifetime(time.Hour)
-	//
-	//postgresDB := databases.NewPostgresDB(db)
-	//
+	logger := initializeLogger()
+	defer func() {
+		err := logger.Sync()
+		if err != nil {
+			log.Fatalf("zap: %v", err)
+		}
+	}()
 
 	config, err := pgxpool.ParseConfig(dsn)
 	if err != nil {
 		log.Fatal(err)
 	}
+	config.ConnConfig.Logger = zapadapter.NewLogger(logger)
+	config.ConnConfig.LogLevel = pgx.LogLevelInfo
 	config.MaxConns = 50
 	config.MaxConnLifetime = time.Minute * 10
 	config.MaxConnIdleTime = time.Minute * 30
 
-	pool, err := pgxpool.Connect(context.Background(), dsn)
+	pool, err := pgxpool.ConnectConfig(context.Background(), config)
 
 	pgxDB := databases.NewPgxDB(pool)
 
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gofiber/fiber/v2 v2.38.1
 	github.com/jackc/pgx/v4 v4.17.2
 	github.com/joho/godotenv v1.4.0
+	go.uber.org/zap v1.23.0
 	gorm.io/driver/postgres v1.4.5
 	gorm.io/gorm v1.24.1-0.20221019064659-5dd2bb482755
 )
@@ -26,6 +27,8 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.40.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
+	go.uber.org/atomic v1.10.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
 	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
 	golang.org/x/sys v0.1.0 // indirect
 	golang.org/x/text v0.3.7 // indirect
